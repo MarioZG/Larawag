c7a593551deb1f0863a35a377b7aeed7bd0a0641
MarioZG <MarioZG@users.noreply.github.com>
2018-01-04 22:56:51 +0000

    Initial folders structure

--- Src/Larawag/EarlyBoundStaticDriver/EarlyBoundStaticDriver.cs
+++ Src/Larawag/EarlyBoundStaticDriver/EarlyBoundStaticDriver.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Larawag
+namespace Larawag.EarlyBoundDriver
 {
-    public class Class1
+    class EarlyBoundDriver
     {
     }
 }
