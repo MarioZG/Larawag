54609576bc34a1be02004344c111af25e4eec747
MarioZG <MarioZG@users.noreply.github.com>
2018-01-13 22:52:32 +0000

    build scripts

---
+++ Build/release.bat
@@ -0,0 +1,7 @@
+"c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" build.xml /fl /target:ReleaseLatest
+echo off
+if %errorlevel% gtr 0 goto error
+echo on
+rem "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" build.xml /target:CleanUp
+:error
+pause
