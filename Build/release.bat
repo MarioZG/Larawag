c0d3a93ef239dbb09a2942818069f9ddd6f64ae9
MarioZG <MarioZG@users.noreply.github.com>
2025-09-14 16:26:12 +0100

    cleanup

--- Build/release.bat
+++ Build/release.bat
@@ -1,7 +1,7 @@
-"c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" build.xml /fl /target:ReleaseLatest -restore
+"MSBuild.exe" build.xml /fl /target:ReleaseLatest -restore
 echo off
 if %errorlevel% gtr 0 goto error
 echo on
-"c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" build.xml /target:CleanUp -restore
+"MSBuild.exe" build.xml /target:CleanUp -restore
 :error
 pause
